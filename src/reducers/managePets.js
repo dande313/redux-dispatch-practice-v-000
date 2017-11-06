fc36059d8ae0c9ab331395f0d9d5c8f0b90545dc
Allen Anderson <dande313@gmail.com>
2017-11-06 04:37:09 +0000

    automatically backed up by learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,7 +1,15 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}){
+  switch(action.type) {
+    case 'ADD_PET':
+      return { pets: [...state.pets, action.pet]}
+    case 'REMOVE_PET':
+      return {pets: state.pets.filter(pet => pet.id !== action.id)}
+    default:
+      return state
+  } 
 }
 
 export function dispatch(){
